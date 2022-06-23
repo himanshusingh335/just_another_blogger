20f287d78594003ca9e3deda3b92b8e0e9025be2
Himanshu Singh <61236944+himanshusingh335@users.noreply.github.com>
2022-06-23 17:31:24 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:just_another_blogger/constants.dart';
+import 'package:just_another_blogger/screens/Login/login_screen.dart';
+import 'package:just_another_blogger/screens/Signup/signup_screen.dart';
+
+import 'screens/Welcome/welcome_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
+      theme: ThemeData(
+          primaryColor: kPrimaryColor,
+          scaffoldBackgroundColor: Colors.white,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+              primary: kPrimaryColor,
+              shape: const StadiumBorder(),
+              maximumSize: const Size(double.infinity, 56),
+              minimumSize: const Size(double.infinity, 56),
+            ),
+          ),
+          inputDecorationTheme: const InputDecorationTheme(
+            filled: true,
+            fillColor: kPrimaryLightColor,
+            iconColor: kPrimaryColor,
+            prefixIconColor: kPrimaryColor,
+            contentPadding: EdgeInsets.symmetric(
+                horizontal: defaultPadding, vertical: defaultPadding),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30)),
+              borderSide: BorderSide.none,
+            ),
+          )),
+      initialRoute: '/',
+      routes: {
+        WelcomeScreen.routeName: (context) => const WelcomeScreen(),
+        LoginScreen.routeName: (context) => const LoginScreen(),
+        SignUpScreen.routeName: (context) => const SignUpScreen()
+      },
+    );
+  }
+}
---
+++ lib/screens/Welcome/components/login_signup_btn.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+import '../../Login/login_screen.dart';
+import '../../Signup/signup_screen.dart';
+
+class LoginAndSignupBtn extends StatelessWidget {
+  const LoginAndSignupBtn({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Hero(
+          tag: "login_btn",
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, LoginScreen.routeName);
+            },
+            child: Text(
+              "Login".toUpperCase(),
+            ),
+          ),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, SignUpScreen.routeName);
+          },
+          style: ElevatedButton.styleFrom(
+              primary: kPrimaryLightColor, elevation: 0),
+          child: Text(
+            "Sign Up".toUpperCase(),
+            style: const TextStyle(color: Colors.black),
+          ),
+        ),
+      ],
+    );
+  }
+}
